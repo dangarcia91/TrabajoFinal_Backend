6c01655bacec2c1af867980244d7a4c400d5ad14
LuisCruzVeldever <luiscruzv@outlook.com>
2023-05-04 23:43:52 -0500

    envio correo

--- .history/aplicativo/sistema/urls_20230504231646.py
+++ .history/aplicativo/sistema/urls_20230504231646.py
@@ -1,13 +1,10 @@
 from django.urls import path
 from .views import *
 from rest_framework_simplejwt.views import TokenObtainPairView
-from mail import views
 
 urlpatterns = [
     path('registro', RegistroUsuario.as_view()),
     path('login', TokenObtainPairView.as_view()),
     path('perfil', Perfilusuario.as_view()),
-    path('productos', ProductoView.as_view()),
-    
-    path('send/', views.Send.as_view(), name='send'),
+    path('productos', ProductoView.as_view())
 ]
