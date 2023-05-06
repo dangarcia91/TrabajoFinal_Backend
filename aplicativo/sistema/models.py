1b6efe827c78cbf7d1eea61399298db7e310fe8d
wilson cutipa <wilson_dani@hotmail.com>
2023-05-06 08:43:42 -0500

    model y views

--- aplicativo/sistema/models.py
+++ aplicativo/sistema/models.py
@@ -39,67 +39,67 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
         db_table = 'usuarios' 
     
     class CategoriasModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    estado = models.BooleanField(default=True)
+        id = models.AutoField(primary_key=True)
+        nombre = models.CharField(max_length=100)
+        estado = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.nombre
+        def __str__(self):
+            return self.nombre
 
-    class Meta:
-        db_table = "categorias"
+        class Meta:
+            db_table = "categorias"
 
-class ProductosModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=250)
-    imagen_url = models.TextField()
-    precio = models.FloatField()
-    estado = models.BooleanField(default=True)
-    categoria_id = models.ForeignKey(CategoriasModel, on_delete=models.CASCADE)
+    class ProductosModel(models.Model):
+        id = models.AutoField(primary_key=True)
+        nombre = models.CharField(max_length=100)
+        descripcion = models.CharField(max_length=250)
+        imagen_url = models.TextField()
+        precio = models.FloatField()
+        estado = models.BooleanField(default=True)
+        categoria_id = models.ForeignKey(CategoriasModel, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.nombre
+        def __str__(self):
+            return self.nombre
 
-    class Meta:
-        db_table = "productos"
+        class Meta:
+            db_table = "productos"
 
-class ClientesModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    correo = models.CharField(max_length=100)
-    dni = models.IntegerField()
+    class ClientesModel(models.Model):
+        id = models.AutoField(primary_key=True)
+        nombre = models.CharField(max_length=100)
+        correo = models.CharField(max_length=100)
+        dni = models.IntegerField()
 
-    def __str__(self):
-        return self.nombre
+        def __str__(self):
+            return self.nombre
 
-    class Meta:
-        db_table = "clientes"
+        class Meta:
+            db_table = "clientes"
 
-class VentasModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    observacion = models.CharField(max_length=100)
-    cliente_id = models.ForeignKey(ClientesModel, on_delete=models.CASCADE)
-    usuario_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    class VentasModel(models.Model):
+        id = models.AutoField(primary_key=True)
+        observacion = models.CharField(max_length=100)
+        cliente_id = models.ForeignKey(ClientesModel, on_delete=models.CASCADE)
+        usuario_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        db_table = "ventas"
+        class Meta:
+            db_table = "ventas"
 
-class DetallesVentaModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    cantidad = models.IntegerField()
-    producto_id = models.ForeignKey(ProductosModel, on_delete=models.CASCADE)
-    venta_id = models.ForeignKey(VentasModel, on_delete=models.CASCADE, related_name='detallesVenta')
+    class DetallesVentaModel(models.Model):
+        id = models.AutoField(primary_key=True)
+        cantidad = models.IntegerField()
+        producto_id = models.ForeignKey(ProductosModel, on_delete=models.CASCADE)
+        venta_id = models.ForeignKey(VentasModel, on_delete=models.CASCADE, related_name='detallesVenta')
 
-    class Meta:
-        db_table = "detalles_venta"
+        class Meta:
+            db_table = "detalles_venta"
 
-class PagosModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    monto = models.FloatField()
-    estado = models.BooleanField(default=True)
-    venta_id = models.ForeignKey(VentasModel, on_delete=models.CASCADE)
+    class PagosModel(models.Model):
+        id = models.AutoField(primary_key=True)
+        monto = models.FloatField()
+        estado = models.BooleanField(default=True)
+        venta_id = models.ForeignKey(VentasModel, on_delete=models.CASCADE)
 
-    class Meta:
-        db_table = "pagos"
+        class Meta:
+            db_table = "pagos"
 
