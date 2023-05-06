fb39eca276a9dae37fdc95787a3ab165e5d060a9
Daniel Garcia <daes_g@hotmail.com>
2023-05-06 01:16:48 -0500

    adición de modelos

--- aplicativo/sistema/models.py
+++ aplicativo/sistema/models.py
@@ -60,10 +60,42 @@ class Producto(models.Model):
 #         return self.nombre
     class Meta:
         db_table = 'productos'
-# VIDEO: minuto 1:18
 
-#login: 1:53 minutos - JSON Web Token
-# pip install djangorestframework-simplejwt
-# pip install cloudinary
-# 1:58:44
-# Views Mascota: 3:08:00
+class ClientesModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+    dni = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = "clientes"
+
+class VentasModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    observacion = models.CharField(max_length=100)
+    cliente_id = models.ForeignKey(ClientesModel, on_delete=models.CASCADE)
+    usuario_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "ventas"
+
+class DetallesVentaModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    cantidad = models.IntegerField()
+    producto_id = models.ForeignKey(ProductosModel, on_delete=models.CASCADE)
+    venta_id = models.ForeignKey(VentasModel, on_delete=models.CASCADE, related_name='detallesVenta')
+
+    class Meta:
+        db_table = "detalles_venta"
+
+class PagosModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    monto = models.FloatField()
+    estado = models.BooleanField(default=True)
+    venta_id = models.ForeignKey(VentasModel, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "pagos"
