db9235b0276241711f1a8a742f91efc1f20a904d
Daniel Garcia <daes_g@hotmail.com>
2023-05-06 09:32:34 -0500

    agregar redirect views

--- aplicativo/aplicativo/urls.py
+++ aplicativo/aplicativo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -34,8 +35,11 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny]
 )
 
+def redirect_view(request):
+    return redirect('/swagger-doc/')
 
 urlpatterns = [
+    path('', redirect_view),
     path('swagger-doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc-doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
